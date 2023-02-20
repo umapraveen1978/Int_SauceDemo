c82c0b9821d0a5c15475e824717a2a0020b934c8
umapraveen1978 <umapraveen1978@gmail.com>
2023-02-20 19:17:00 +0000

    Code Alligned

--- src/test/java/tests/sauceDemo.java
+++ src/test/java/tests/sauceDemo.java
@@ -13,21 +13,21 @@ public class sauceDemo extends BaseTest {
     @BeforeTest
     public void gotoApp() {
         goTo("https://www.saucedemo.com");}
-        @Test
-        public void doOperation() throws InterruptedException{
+    @Test
+    public void doOperation() throws InterruptedException{
 // Login with correct Username and Password
-            sendKeys(By.xpath("//*[@id='user-name']"), "standard_user");
-            sendKeys(By.xpath("//*[@id='password']"), "secret_sauce");
-            click(By.xpath("//*[@id='login-button']"));
+        sendKeys(By.xpath("//*[@id='user-name']"), "standard_user");
+        sendKeys(By.xpath("//*[@id='password']"), "secret_sauce");
+        click(By.xpath("//*[@id='login-button']"));
 
 // Get a list of all product prices
             List<WebElement> allPrices = driver.findElements(By.xpath("//*[@class='inventory_item_price']"));
 
 // Find the highest priced product
-            WebElement highestProduct = allPrices.get(0);
-            double highPrice = Double.parseDouble(highestProduct.getText().replace("$", ""));
-            for (WebElement productPrice : allPrices) {
-                double price = Double.parseDouble(productPrice.getText().replace("$", ""));
+        WebElement highestProduct = allPrices.get(0);
+        double highPrice = Double.parseDouble(highestProduct.getText().replace("$", ""));
+        for (WebElement productPrice : allPrices) {
+            double price = Double.parseDouble(productPrice.getText().replace("$", ""));
                 if (price > highPrice) {
                     highPrice = price;
                     highestProduct = productPrice;
